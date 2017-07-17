f982dc3607d6038da3623c6bf9b3d920f3a1e660
Jonathan Dextraze <jdextraze@xerafloptech.com>
2017-07-17 13:11:38 -0700

    protobuf -> messages

--- generate-protobuf.sh
+++ generate-protobuf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc -I$GOPATH/src --go_out=$GOPATH/src $GOPATH/src/github.com/jdextraze/go-gesclient/protobuf/ClientMessageDtos.proto
+protoc -I$GOPATH/src --go_out=$GOPATH/src $GOPATH/src/github.com/jdextraze/go-gesclient/messages/ClientMessageDtos.proto
